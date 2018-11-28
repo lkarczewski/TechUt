4d2ff99df65e08fb5fbf8eaeb51ed65ea76d9e18
Łukasz Karczewski <lkarczewski@sigma.ug.edu.pl>
2018-11-28 17:36:34 +0100

    Added methods

--- clubDemo/src/main/java/com/example/jdbcdemo/service/ClubService.java
+++ clubDemo/src/main/java/com/example/jdbcdemo/service/ClubService.java
@@ -9,10 +9,20 @@ import com.example.jdbcdemo.domain.Club;
 public interface ClubService {
 	
 	Connection getConnection();
-	void addClub(Club club) throws SQLException;
-	void deleteClub(Club club) throws SQLException;
-	void deleteAllClubs();
+	boolean addClub(Club club) throws SQLException;
+	boolean addClubs(List<Club> clubs) throws SQLException;
+	boolean deleteClub(Club club) throws SQLException;
+	boolean deleteClubs();
 	List<Club> getAllClubs();
+	boolean deleteClubs(List<Club> clubs);
+	boolean selectClubById(long id);
+	boolean selectClubByName(String name);
+	Club getClubById(long id);
+	Club getClubByName(String name);
+	boolean deleteClubById(long id);
+	boolean deleteClubByName(String name);
+	List<Club> getClubsAssets(double min, double max);
+	
 	
 	/* batch insert - transactional */
 	//public void addAllPersons(List<Club> persons);
