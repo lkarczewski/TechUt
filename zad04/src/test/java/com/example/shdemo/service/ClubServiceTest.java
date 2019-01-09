f9a3c9b77f073b53d1672ef33dfe0c724f3ef2a3
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-01-09 17:17:50 +0100

    zad04 testy2

--- zad04/src/test/java/com/example/shdemo/service/ClubServiceTest.java
+++ zad04/src/test/java/com/example/shdemo/service/ClubServiceTest.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/beans.xml"})
 @TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
@@ -30,18 +29,38 @@ public class ClubServiceTest {
 	private final String CLUB_DOF_1 = "31-01-1899";
 	private final boolean CLUB_CHAMPION_1 = true;
 	private final double CLUB_ASSETS_1 = 60000000;
+	
+//	private final String PLAYER_FIRST_NAME_1 = "";
+//	private final String PLAYER_LAST_NAME_1 = "";
+//	private final String PLAYER_NATIONALITY_1 = "";
+//	private final Club PLAYER_CLUB_1;
+	
 
 	@Test
 	public void addClubTest() {
+		
+		List<Club> retrievedClubs = clubService.getAllClubs();
+		
+		for (Club club : retrievedClubs) {
+			if (club.getName().equals(CLUB_NAME_1)) {
+				clubService.deleteClub(club);
+			}
+		}
+		
 		Club club = new Club();
 		club.setName(CLUB_NAME_1);
 		club.setDateOfFoundation(CLUB_DOF_1);
 		club.setChampion(CLUB_CHAMPION_1);
 		club.setAssets(CLUB_ASSETS_1);
-
-		assertEquals(CLUB_NAME_1, club.getName());
-		assertEquals(CLUB_DOF_1, club.getDateOfFoundation());
-		assertEquals(CLUB_CHAMPION_1, club.isChampion());
-		//assertEquals(CLUB_ASSETS_1, club.getAssets());
+		
+		clubService.addClub(club);
+		
+		Club retrievedClub = clubService.findClubById(club.getId());
+		assertEquals(CLUB_NAME_1, retrievedClub.getName());
+		assertEquals(CLUB_DOF_1, retrievedClub.getDateOfFoundation());
+		assertEquals(CLUB_CHAMPION_1, retrievedClub.isChampion());
+		assertEquals(CLUB_ASSETS_1, retrievedClub.getAssets(), 0);
 	}
+	
+	@Test
 }
